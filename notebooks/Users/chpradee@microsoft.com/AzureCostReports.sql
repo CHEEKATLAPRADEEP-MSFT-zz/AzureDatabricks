e8f2e8fa07c28e3f3acd752597eb7984e526217d
CHEEKATLAPRADEEP-MSFT <44426401+CHEEKATLAPRADEEP-MSFT@users.noreply.github.com>
2019-10-17 19:42:18 +0530

    Update: README.md notebooks

---
+++ notebooks/Users/chpradee@microsoft.com/AzureCostReports.sql
@@ -0,0 +1,19 @@
+-- Databricks notebook source
+-- MAGIC %md # This notebook is dedicated to Azure Cost Analysis Reports
+
+-- COMMAND ----------
+
+-- MAGIC %md 
+-- MAGIC <img src="https://chepra.blob.core.windows.net/images/GetData.jpg"/></a>
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC <img src="http://docs.databricks.com/_static/images/getting-started/quickstart-cluster.png"/></a>
+
+-- COMMAND ----------
+
+-- MAGIC %md ![Azurewala](https://chepra.blob.core.windows.net/images/GetData.jpg)
+
+-- COMMAND ----------
+
